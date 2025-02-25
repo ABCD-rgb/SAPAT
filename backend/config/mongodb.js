a9399cb66bdc7185b00109dfa71593f201261a33
ABCD-rgb <agdelmo1@up.edu.ph>
2025-02-25 21:45:55 +0800

    chore: heroku stuff

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -8,7 +8,8 @@ const handleMongoDB = async () => {
         await mongoose.connect(process.env.MONGODB_URI);
         console.log("Connected to MongoDB");
     } catch (error) {
-        console.log("Error connecting to MongoDB", error);
+        console.error('MongoDB connection error:', error);
+        process.exit(1); // Exit if cannot connect to database
     }
 };
 
