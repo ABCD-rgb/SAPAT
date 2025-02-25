60d7f909f2b302ffd8764d98d5cb0fe8045ea2d1
ABCD-rgb <agdelmo1@up.edu.ph>
2025-02-25 15:22:42 +0800

    feat: initial setup of Google auth

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import cors from "cors";
+import dotenv from 'dotenv';
 import { createServer } from "http";
 import { Server } from "socket.io";
-import handleRoutes from './router.js'
-import handleSocket from './config/socket.js'
-import handleMongoDB from './config/mongodb.js'
+import handleRoutes from './router.js';
+import handleSocket from './config/socket.js';
+import handleMongoDB from './config/mongodb.js';
+import passport from './config/auth.js';
+import session from 'express-session';
 
+dotenv.config();
 const app = express();
 app.use(express.json());
 
@@ -23,6 +27,40 @@ const io = new Server(httpServer, {
 handleMongoDB();
 handleRoutes(app);
 io.on("connection", handleSocket);
+
+
+// auth
+app.use(session({
+  secret: 'your_session_secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { 
+    secure: false, // set to true if using https
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/auth/google',
+  passport.authenticate('google', { 
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/login/failed',
+    successRedirect: 'http://localhost:5173/dashboard'
+  })
+);
+
+app.get('/login/failed', (req, res) => {
+  res.status(401).json({
+    success: false,
+    message: "failure",
+  });
+});
 
 httpServer.listen(3000, () => {
   console.log(`Server is running on port 3000`);
--- frontend/SAPAT/src/pages/Login.jsx
+++ frontend/SAPAT/src/pages/Login.jsx
@@ -1,6 +1,11 @@
 import { Link } from 'react-router-dom'
 
 function Login() {
+
+  const handleGoogleLogin = () => {
+    window.location.href = 'http://localhost:3000/auth/google';
+  }
+
   return (
     <div className="min-h-screen w-full">
       {/* Mobile background - hidden on desktop */}
@@ -82,7 +87,11 @@ function Login() {
               </div>
             </div>
 
-            <button className="mt-4 flex w-full items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50">
+            <button 
+              onClick={handleGoogleLogin}
+              type="button"
+              className="mt-4 flex w-full items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50"
+            >
               <img
                 className="mr-2 h-5 w-5"
                 src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
